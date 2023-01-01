8623b3f71b8f8a6646ab26afed988df079dcdd83
Chris Marshall <thoughtpalettechris@gmail.com>
2022-12-31 18:10:59 -0600

    Removed commented out code

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,6 @@ struct Todo {
 
 #[function_component]
 fn App() -> Html {
-    // let counter = use_state(|| 0);
-    // let onclick = {
-    //     let counter = counter.clone();
-    //     move |_| {
-    //         let value = *counter + 1;
-    //         counter.set(value);
-    //     }
-    // };
-
-    // let initial_todos = HashMap::Todo::new();
-    // initial_todos.insert(1, "Test");
-    // let display_todos = todos.iter();
-
     let todos = use_state(|| vec!["Todo", "Todo2"]);
 
     html! {
