00c3486294bc866f9bdbf145c638797ea7564c11
Chris Marshall <thoughtpalettechris@gmail.com>
2022-12-31 18:05:17 -0600

    Actually able to add todo item to use state vector now :}

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ struct Todo {
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
+    // let counter = use_state(|| 0);
+    // let onclick = {
+    //     let counter = counter.clone();
+    //     move |_| {
+    //         let value = *counter + 1;
+    //         counter.set(value);
+    //     }
+    // };
 
     // let initial_todos = HashMap::Todo::new();
     // initial_todos.insert(1, "Test");
@@ -30,7 +30,15 @@ fn App() -> Html {
                 { for todos.iter().map(|todo| html! { <li>{todo}</li> }) }
             </ul>
             <footer>
-                <button {onclick}>{ "Add" }</button>
+                <button onclick={move |_evt| {
+                    let state = todos.clone();
+                    let mut todos = todos.to_vec();
+
+                    todos.push("yeet");
+                    state.set(todos);
+                }}>
+                    { "Add" }
+                </button>
             </footer>
         </main>
     }
